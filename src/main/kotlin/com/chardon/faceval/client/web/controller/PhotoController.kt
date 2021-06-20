4d75dc07253304b1a81bd175052fa9fa003aabdd
chardon55 <charlesdong_2000@outlook.com>
2021-06-21 07:50:36 +0800

    Add strip base option

--- src/main/kotlin/com/chardon/faceval/client/web/controller/PhotoController.kt
+++ src/main/kotlin/com/chardon/faceval/client/web/controller/PhotoController.kt
@@ -3,15 +3,13 @@ package com.chardon.faceval.client.web.controller
 import com.chardon.faceval.client.web.service.PhotoService
 import com.chardon.faceval.entity.PhotoInfo
 import com.chardon.faceval.entity.PhotoInfoUpdate
-import com.chardon.faceval.entity.PhotoInfoUpload
 import com.chardon.faceval.entity.PhotoInfoUploadBase64
 import feign.FeignException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.multipart.MultipartFile
+import java.util.stream.Collectors
 
 @RestController
 @RequestMapping("/photo")
@@ -22,22 +20,48 @@ class PhotoController {
 
     @GetMapping("/")
     fun getPhoto(@RequestParam("photo_id") photoId: Long?,
-                 @RequestParam("user_id") userId: String?): ResponseEntity<List<PhotoInfo>> {
+                 @RequestParam("user_id") userId: String?,
+                 @RequestParam("attach_base") attachBase: Boolean = false): ResponseEntity<List<PhotoInfo>> {
         return try {
-            ResponseEntity(photoService.getPhoto(photoId, userId), HttpStatus.OK)
+            val photos = photoService.getPhoto(photoId, userId)
+
+            if (!attachBase) {
+                val updatedPhotos = photos.stream().map {
+                    it.stripBase()
+                }.collect(Collectors.toList())
+
+                return ResponseEntity(updatedPhotos, HttpStatus.OK)
+            }
+
+            ResponseEntity(photos, HttpStatus.OK)
         } catch (e: FeignException.NotFound) {
             ResponseEntity(null, HttpStatus.NOT_FOUND)
         }
     }
 
     @PostMapping("/")
-    fun createPhoto(newPhoto: PhotoInfoUploadBase64): PhotoInfo {
-        return photoService.addPhoto(newPhoto)
+    fun createPhoto(
+        newPhoto: PhotoInfoUploadBase64,
+        @RequestParam("attach_base") attachBase: Boolean = false
+    ): PhotoInfo {
+        val addedPhoto = photoService.addPhoto(newPhoto)
+
+        return if (attachBase) {
+            addedPhoto
+        } else {
+            addedPhoto.stripBase()
+        }
     }
 
     @PutMapping("/")
-    fun updatePhoto(updatedPhoto: PhotoInfoUpdate): PhotoInfo {
-        return photoService.updatePhoto(updatedPhoto)
+    fun updatePhoto(updatedPhoto: PhotoInfoUpdate,
+                    @RequestParam("attach_base") attachBase: Boolean = false): PhotoInfo {
+        val updatedPhoto1 = photoService.updatePhoto(updatedPhoto)
+        return if (attachBase) {
+            updatedPhoto1
+        } else {
+            updatedPhoto1.stripBase()
+        }
     }
 
     @DeleteMapping("/")
@@ -45,5 +69,20 @@ class PhotoController {
                     @RequestParam("password") password: String,
                     @RequestParam("photo_id") photoId: Long): Map<String, String> {
         return photoService.removePhoto(userName, password, photoId)
+    }
+
+    private fun PhotoInfo.stripBase(): PhotoInfo {
+        return this.let {
+            PhotoInfo(
+                base = "",
+                dateAdded = it.dateAdded,
+                description = it.description,
+                id = it.id,
+                positions = it.positions,
+                score = it.score,
+                tag = it.tag,
+                title = it.title,
+            )
+        }
     }
 }
