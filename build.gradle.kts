150bd6f1e8bbf00e01a71ef566f9c450a3f043ce
chardon55 <charlesdong_2000@outlook.com>
2021-06-21 08:16:08 +0800

    Update scoring logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "com.chardon.faceval"
-version = "1.0.4-MAINLINE"
+version = "1.2.0-MAINLINE"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 configurations {
--- src/main/kotlin/com/chardon/faceval/client/web/util/ScoringProcessor.kt
+++ src/main/kotlin/com/chardon/faceval/client/web/util/ScoringProcessor.kt
@@ -3,6 +3,7 @@ package com.chardon.faceval.client.web.util
 import com.chardon.faceval.entity.ScoringResult
 import org.springframework.stereotype.Component
 import java.util.*
+import kotlin.random.Random
 
 @Component
 class ScoringProcessor {
@@ -12,14 +13,28 @@ class ScoringProcessor {
     private val baseScore = 6.0
 
     protected fun scoringSingleFace(face: List<Double>,
-                                    eyes: Pair<List<Double>, List<Double>>,
-                                    nose: List<Double>,
-                                    mouth: List<Double>): Double {
+                                    eyes: Pair<List<Double>, List<Double>>?,
+                                    nose: List<Double>?,
+                                    mouth: List<Double>?): Double {
         // Face part
         val faceScore = face[1] - face[0]
 
+        if (eyes == null || nose == null || mouth == null) {
+            if (face.isEmpty()) {
+                return baseScore * scale
+            }
+
+            val weighted = (faceScore + baseScore) * scale
+            return when {
+                weighted < 0 -> 0.0
+                weighted > 10 -> 10.0
+                else -> weighted
+            }
+        }
+
         // Eyes part
         var eyesScore = 0.0
+
 
         val eyesList = listOf(eyes.first, eyes.second)
         for (item in eyesList) {
@@ -36,7 +51,7 @@ class ScoringProcessor {
 
         val rawWeight = faceScore * 0.5 + eyesScore * 0.2 + noseScore * 0.1 + mouthScore * 0.1
 
-        val rawScore = baseScore + rawWeight * scale
+        val rawScore = (baseScore + rawWeight) * scale
 
         return when {
             rawScore < 0 -> 0.0
@@ -45,12 +60,25 @@ class ScoringProcessor {
         }
     }
 
+    private val seed = Random(129)
+
     fun getScore(scoringResult: ScoringResult): List<Double> {
         val faceCount = scoringResult.face.count()
+
+        if (faceCount <= 0) {
+            // If there is no face at all... random a score (because I don't you where are you!!!)
+            return listOf(seed.nextDouble(8.0, 9.8))
+        }
 
         val scores = LinkedList<Double>()
 
         for (offset in 0 until faceCount) {
+            if (scoringResult.eye.size <= offset * 2 + 1 ||
+                scoringResult.nose.size <= offset ||
+                scoringResult.mouth.size <= offset) {
+                scores.add(scoringSingleFace(scoringResult.face[offset], null, null, null))
+            }
+
             scores.add(
                 scoringSingleFace(
                     scoringResult.face[offset],
